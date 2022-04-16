9689f35647f0f1461d118881d1db4886ddb7fda4
NinhaRilva <dyvartes@gmail.com>
2022-04-15 23:02:15 -0300

    sellerService Criando

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Service;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
 
     public class SellersController : Controller
     {
-    private readonly SellerService _sallerService;
+        // quando é usado readonly para previne que
+        //essa dependencia não seja alterada 
+
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
         public IActionResult Index()
         {
-            var list = _sallerService;
-            return View();
+            var list = _sellerService.FindAll();
+             
+            return View(list);
         }
     }
 }
